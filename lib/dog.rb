5bff98da6fa0b96d5f6e5c9d55e9fc96f23a0205
Vanessa Toro <vanetoro84@gmail.com>
2018-03-16 14:03:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  
+  def name=(dog_name)
+    @name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark=(dog_bark)
+    @bark
+  end
+  
+  def bark 
+    @bark
+  end
+end
